c71b68cc432e138f1f1dc968f6826e645d4772d7
Mark Clarke <58795613+MClarke9k9@users.noreply.github.com>
2022-03-02 18:29:16 -0500

    First project demo push

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # BeEazy
-This is my VS Code extension which helps you search what you need for React without opening your browser from your VS code search bar.
+This is my VS Code extension which helps you search what you need for React without opening your browser, but instead from your VS code search bar.
---
+++ beeazy/extension.js
@@ -0,0 +1,88 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+const theList = require('./reactjuice.json');
+// const jp = require('jsonparser');
+// const app = require('express')();
+// const htmlparser = require('html-parser');
+// const axios = require('axios');
+
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+    const reactInfo = theList.map( theList => {
+		return {
+		label: theList.title,
+		detail: theList.description,
+		link: theList.link
+		}
+	})
+	
+	// var infoLink = reactInfo.link;
+	console.log('Congratulations, your extension "beeazy" is now active!');
+	console.log(reactInfo);
+
+	let disposable = vscode.commands.registerCommand('beeazy.helloWorld', function () {
+		
+
+		// var theLink = vscode.env.openExternal(reactInfo.link)
+		const firstOne = vscode.window.showQuickPick(reactInfo, {
+			 matchOnDetail: true,
+			//  onDidSelectItem: (reactInfo) => {
+			// 	 vscode.env.openExternal(reactInfo.link)
+			//  }
+		 }).then(() => {
+			//  if(reactInfo == null) return
+			 onDidSelectItem: vscode.env.openExternal(reactInfo.link)
+			})
+		 
+		//  .then((selection) => {
+		// 	 if (reactInfo !== null) return
+		//  })
+		
+		
+
+	});
+
+	// let disposableTwo = vscode.commands.registerCommand('beeazy.helloWorld', function () {
+	// 	const reactInfo = theList.map( theList => {
+	// 		return {
+	// 		label: theList.title,
+	// 		detail: theList.description,
+	// 		link: theList.link
+	// 		}
+	// 	})
+
+	// 	// var theLink = vscode.env.openExternal(reactInfo.link)
+	// 	const theQuestion = vscode.window.showQuickPick(reactInfo, {
+	// 		 matchOnDetail: true,
+	// 		 onDidSelectItem: (reactInfo) => {
+	// 			 vscode.env.openExternal(reactInfo.link)
+	// 		 }
+	// 	 })
+		 
+	// 	//  .then((selection) => {
+	// 	// 	 if (reactInfo !== null) return
+	// 	//  })
+		
+		
+	
+	// });
+	
+	// context.subscriptions.push(disposableTwo);
+
+	context.subscriptions.push(disposable);
+}
+
+
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
